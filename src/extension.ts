8ad9b05ecc21767075f7a1ffcbeb0bf4a3fb9381
AperanoGavin <audesandrine6@gmail.com>
2023-01-24 23:25:07 +0100

    add voice check

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const speech = require('@google-cloud/speech');
+const client = new speech.SpeechClient();
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	//demander à l'utilisateur si vscode peut accéder à son micro
+	vscode.window.showInformationMessage('Do you want to allow VSCode to access your microphone?', 'Yes', 'No').then((answer) => {
+		if (answer === 'Yes') {
+			//si l'utilisateur accepte, on lance la fonction qui va écouter le micro
+			
+		}
+	}
+	
+	
+	
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gavin-voice-vscode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gavin-voice-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gavin-voice-vscode!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
